a92f95ca502a6e7e2ef35255d733fbdba0a8d30e
Robert Bassi <rbrt.bassi01@gmail.com>
2024-07-31 19:21:54 +0100

    add initial js to open and close modal

--- shared.js
+++ shared.js
@@ -2,7 +2,33 @@
 
 // work on modal:
 
+// assigned-variables:
 let backdrop = document.querySelector(".backdrop");
+let selectPlanButtons = document.querySelectorAll(".plan button");
+let modal = document.querySelector(".modal");
+// close modal-action
+let modalBtnNo = document.querySelector(".modal__action--negative");
 
-// console.dir(backdrop);
-backdrop.style.display = "block";
+// named function: open modal
+function openModal() {
+  modal.style.display = "block";
+  backdrop.style.display = "block";
+}
+
+// named function: close modal
+function closeModal() {
+  modal.style.display = "none";
+  backdrop.style.display = "none";
+}
+
+// check if selections is right:
+console.dir(modal);
+// ====================
+// open modal:
+for (let i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener("click", openModal);
+}
+// close modal by clicking on "No":
+modalBtnNo.addEventListener("click", closeModal);
+// close modal by clicking on "Background":
+backdrop.addEventListener("click", closeModal);
