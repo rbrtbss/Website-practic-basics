dc271e05c7784b485c9a02bcfc01d47a9a0f572e
Robert Bassi <rbrt.bassi01@gmail.com>
2024-08-03 22:33:28 +0100

    fine tune display:block for js

--- shared.js
+++ shared.js
@@ -9,19 +9,32 @@
 let backdrop = document.querySelector(".backdrop");
 let selectPlanButtons = document.querySelectorAll(".plan button");
 let modal = document.querySelector(".modal");
+
 // close modal-action
 let modalBtnNo = document.querySelector(".modal__action--negative");
 
+// Toggle bar variables:
+let toggleBtn = document.querySelector(".toggle-btn");
+let mobileNav = document.querySelector(".mobile-nav");
+
+// NAMED FUNCTIONS:
 // named function: open modal
 function openModal() {
   modal.style.display = "block";
   backdrop.style.display = "block";
 }
-
 // named function: close modal
 function closeModal() {
   modal.style.display = "none";
   backdrop.style.display = "none";
+}
+// named function: mobile nav
+function openNav() {
+  mobileNav.style.display = "block";
+  backdrop.style.display = "block";
+}
+function closeNav() {
+  mobileNav.style.display = "none";
 }
 
 // check if selections is right:
@@ -33,5 +46,12 @@ for (let i = 0; i < selectPlanButtons.length; i++) {
 }
 // close modal by clicking on "No":
 modalBtnNo.addEventListener("click", closeModal);
+
 // close modal by clicking on "Background":
 backdrop.addEventListener("click", closeModal);
+
+// addEventListener to open mobile navigation:
+toggleBtn.addEventListener("click", openNav);
+
+// close mobileNavigation by clicking on "Background":
+backdrop.addEventListener("click", closeNav);
